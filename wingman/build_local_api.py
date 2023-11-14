8b1a2380aed133a5f17cdf9f9e6d059f1a5236b7
SergeyIvanov87 <sergius140608@rambler.ru>
2023-11-14 23:51:13 +0100

    Add local API generation stages, add wingman.org API fs entry point

--- wingman/build_local_api.py
+++ wingman/build_local_api.py
@@ -9,14 +9,50 @@ def make_file_executable(file):
     st = os.stat(file)
     os.chmod(file, st.st_mode | stat.S_IEXEC)
 
+def compose_api_fs_node_name(api_root, req):
+    api_node = os.path.join(api_root, req)
+    return api_node
+
+def create_api_fs_node(api_root, req, req_type):
+    api_node = compose_api_fs_node_name(api_root, req)
+    os.makedirs(api_node, exist_ok=True);
+    api_node_leaf = os.path.join(api_node, req_type.lower())
+
+    with open(api_node_leaf, "w") as api_leaf_file:
+        make_file_executable(api_node_leaf)
+        api_leaf_file.write("0\n")    # event `access` in notify can't be trigger on empty file
+    return api_node
+
+def compose_script_for_dev_name(script_name):
+    return f"{script_name}_exec.sh"
+
+def create_script_for_dev(path, script_name):
+    script_name_generated = compose_script_for_dev_name(script_name)
+    script_generated_path = os.path.join(path, script_name_generated)
+    with open(script_generated_path, "w") as script:
+        make_file_executable(script_generated_path)
+        script.write("#!/usr/bin/bash\n\n. ${1}/setenv.sh\n<TODO>")
+
+    return script_name_generated
+
 parser = argparse.ArgumentParser(
     prog="Build file-system API nodes based on pseudo-REST API from cfg file"
 )
+
+EXEC_MODE = ["devel", "build", "run"]
+EXEC_MODE_DEV=0
+EXEC_MODE_HOST=1
+EXEC_MODE_IMAGE=2
+
+parser.add_argument(
+    "mode",
+    help=f"Script execution mode: {EXEC_MODE}"
+)
+
 parser.add_argument(
     "api_file",
     help="Path to file with API description"
 )
-
 
 parser.add_argument(
     "mount_point",
@@ -25,14 +61,17 @@ parser.add_argument(
 
 
 args = parser.parse_args()
+if args.mode not in EXEC_MODE:
+    raise Exception("Invalid execution mode: {}. Supported values: {}".format(args.mode, EXEC_MODE))
 
-api_schema = ["inotifywait -m {0} -e access --include 'get' |\n",
+api_schema = [". ${1}/setenv.sh\n",
+"shopt -s extglob\n",
+"inotifywait -m {0} -e access --include 'get' |\n",
 "\twhile read dir action file; do\n",
 "\t\techo \"file: ${file}, action; ${action}, dir: ${dir}\"\n",
 "\t\tcase \"$action\" in\n",
 "\t\t\tACCESS|ATTRIB )\n",
-"\t\t\t\t\"${WORK_DIR}/",
-"<generate>",
+"\t\t\t\t\"${0}/{1}\" ${2}\n",
 "\t\t\t;;\n",
 "\t\t\t*)\n",
 "\t\t\t\t;;\n",
@@ -40,7 +79,7 @@ api_schema = ["inotifywait -m {0} -e access --include 'get' |\n",
 "\tdone\n"
 ]
 
-generated_api_server_scripts_path = "generated"
+generated_api_server_scripts_path = "services"
 os.makedirs(generated_api_server_scripts_path, exist_ok=True);
 
 with open(args.api_file, 'r') as api_file:
@@ -52,22 +91,35 @@ with open(args.api_file, 'r') as api_file:
             (req_name, req_type, req_api) = request_params
 
             '''re-create filesystem directory structure based on API query'''
-            api_node = os.path.join(args.mount_point, req_api)
-            os.makedirs(api_node, exist_ok=True);
-            api_node_leaf = os.path.join(api_node, req_type.lower())
+            '''must be done in 'run' mode only'''
+            if args.mode == EXEC_MODE[EXEC_MODE_IMAGE]:
+                api_node = create_api_fs_node(args.mount_point, req_api, req_type)
+                continue
+            else:
+                api_node = compose_api_fs_node_name(args.mount_point, req_api)
 
-            with open(api_node_leaf, "w") as api_leaf_file:
-                make_file_executable(api_node_leaf)
-                api_leaf_file.write("0\n")    # event `access` in notify can't be trigger on empty file
+            '''in 'devel' mode this builder must generate stub files for API request to implement'''
+            '''must be done BEFORE docker image crafted'''
+            if args.mode == EXEC_MODE[EXEC_MODE_DEV]:
+                req_executor_name = create_script_for_dev(generated_api_server_scripts_path, req_name)
+            else:
+                req_executor_name = compose_script_for_dev_name(req_name)
 
-                api_server_script_file_name = f"{req_name}_listener.sh"
-                api_server_script_file_path = os.path.join(generated_api_server_scripts_path, api_server_script_file_name)
-                with open(api_server_script_file_path, "w") as listener_file:
-                    api_schema_concrete = api_schema.copy()
-                    api_schema_concrete[0] = api_schema_concrete[0].format(api_node)
-                    api_schema_concrete[6] = f"{req_name}_exec.sh\"\n"
 
-                    listener_file.write("#!/usr/bin/bash\n\n")
-                    listener_file.writelines(api_schema_concrete)
+            '''must not generate served listeners scripts in 'run' mode'''
+            api_server_script_file_name = f"{req_name}_listener.sh"
+            api_server_script_file_path = os.path.join(generated_api_server_scripts_path, api_server_script_file_name)
 
-                    make_file_executable(api_server_script_file_path)
+            '''In dev mode generate it in testing purposes'''
+            if args.mode == EXEC_MODE[EXEC_MODE_DEV]:
+                api_server_script_file_path += ".test_only"
+
+            with open(api_server_script_file_path, "w") as listener_file:
+                api_schema_concrete = api_schema.copy()
+                api_schema_concrete[2] = api_schema_concrete[2].format(api_node)
+                api_schema_concrete[7] = api_schema_concrete[7].format("{WORK_DIR}", req_executor_name, "{WORK_DIR}")
+
+                listener_file.write("#!/usr/bin/bash\n\n")
+                listener_file.writelines(api_schema_concrete)
+
+            make_file_executable(api_server_script_file_path)
--- wingman/init.sh
+++ wingman/init.sh
@@ -2,18 +2,25 @@
 
 WORK_DIR=${1}
 REPO_PATH=${2}
-SHARED_DIR=${2}/pmccabe
+SHARED_DIR=${2}
+SHARED_API_DIR=${2}/wingman.org
 
-echo -e "#!/usr/bin/bash\n\nexport WORK_DIR=${WORK_DIR} \nexport SHARED_DIR=${SHARED_DIR} \nexport REPO_PATH=${REPO_PATH}" > ${WORK_DIR}/setenv.sh
+echo -e "#!/usr/bin/bash\n\nexport WORK_DIR=${WORK_DIR} \nexport SHARED_DIR=${SHARED_DIR} \nexport SHARED_API_DIR=${SHARED_API_DIR} \nexport REPO_PATH=${REPO_PATH}" > ${WORK_DIR}/setenv.sh
 
-#allow wingman to access reposiroty
+# allow wingman to access reposiroty
 git config --global --add safe.directory ${REPO_PATH}
 
-mkdir -p ${SHARED_DIR}
+# create API directory and initialize API nodes
+mkdir -p ${SHARED_API_DIR}
+${WORK_DIR}/build_local_api.py "run" ${WORK_DIR}/API.replicated.fs ${SHARED_DIR}
 
 # create pivot metrics
-${WORK_DIR}/build_pmccabe_xml.sh "${SHARED_DIR}/init.xml"
-${WORK_DIR}/build_pmccabe_flamegraph.sh "${SHARED_DIR}/init.xml" "${SHARED_DIR}/init.svg"
+${WORK_DIR}/build_pmccabe_xml.sh "${SHARED_API_DIR}/init.xml"
+${WORK_DIR}/build_pmccabe_flamegraph.sh "${SHARED_API_DIR}/init.xml" "${SHARED_API_DIR}/init.svg"
 
 # run listener for events
-${WORK_DIR}/listen.sh ${SHARED_DIR}
+for s in ${WORK_DIR}/services/*.sh; do
+    ${s} ${WORK_DIR} ${SHARED_DIR} &
+done
+
+sleep infinity
