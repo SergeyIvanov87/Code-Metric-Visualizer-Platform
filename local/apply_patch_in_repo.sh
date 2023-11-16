7b738500936b38b9ee590d68886262363f8ae15e
SergeyIvanov87 <sergius140608@rambler.ru>
2023-11-16 10:10:25 +0100

    Remove superfluous files

--- local/apply_patch_in_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-. /package/setenv.sh
-echo "file with patch ${1}"
-cd ${REPO_PATH}
-cp ${SHARED_DIR}/${1} ./
-patch -p1 < ${1} 
-
--- local/reset_existing_repo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-. /package/setenv.sh
-
-cd ${REPO_PATH}
-git reset --hard origin/${REPO_BRANCH}
-
