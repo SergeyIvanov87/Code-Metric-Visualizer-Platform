dca84f203d864275e7dedb0bd69c8e4cedbb4302
SergeyIvanov87 <sergius140608@rambler.ru>
2024-01-10 10:04:51 +0100

    Employ Arguments bypassing in RRD, change for API: rrd_graph_view -> rrd_plot_view

--- submodules/rrd/local/api_generator.py
+++ submodules/rrd/local/api_generator.py
@@ -34,7 +34,7 @@ def make_script_rrd_collect(script):
         *api_generator_utils.generate_get_result_type(""), r"",
         *api_generator_utils.generate_api_node_env_init(), r"",
         *api_generator_utils.generate_read_api_fs_args(), r"",
-        r'echo 0 > ${SHARED_API_DIR}/analytic/rrd/PUT/exec',
+        r'echo ${IN_ARGS[@]} > ${SHARED_API_DIR}/analytic/rrd/PUT/exec',
         r'RRD_DB_PARAMS=`cat ${SHARED_API_DIR}/analytic/rrd/PUT/result`',
         r'echo "-mmcc=1 -tmcc=1 -sif=1 -lif=1" > ${SHARED_API_DIR}/main/statistic/GET/exec',
         r'cat ${SHARED_API_DIR}/main/statistic/GET/result.xml | ${WORK_DIR}/build_rrd.py "${RRD_DB_PARAMS}" ${SHARED_API_DIR} ${brr[@]}',
@@ -62,7 +62,7 @@ def make_script_rrd_view(script):
         *api_generator_utils.generate_exec_header(), r"",
         *api_generator_utils.generate_get_result_type(".csv"), r"",
         *api_generator_utils.generate_api_node_env_init(), r"",
-        r'echo 0 > ${SHARED_API_DIR}/analytic/rrd/select/PUT/exec',
+        r'echo "${IN_ARGS[@]}" > ${SHARED_API_DIR}/analytic/rrd/select/PUT/exec',
         r'cat ${SHARED_API_DIR}/analytic/rrd/select/PUT/result | xargs find ${RRD_ROOT} | ${WORK_DIR}/fetch_rrd.py ${SHARED_API_DIR}/analytic/rrd/select/view',
     )
     script.writelines(line + "\n" for line in body)
@@ -76,8 +76,8 @@ def make_script_rrd_view_graph(script):
         *api_generator_utils.generate_get_result_type(".png"), r"",
         *api_generator_utils.generate_api_node_env_init(), r"",
         r"RESULT_FILE=`mktemp -u`",
-        r'echo 0 > ${SHARED_API_DIR}/analytic/rrd/select/PUT/exec',
-        r'cat ${SHARED_API_DIR}/analytic/rrd/select/PUT/result | xargs find ${RRD_ROOT} | ${WORK_DIR}/graph_rrd.py ${SHARED_API_DIR}/analytic/rrd/select/view/graph ${RESULT_FILE}',
+        r'echo "${IN_ARGS[@]}" > ${SHARED_API_DIR}/analytic/rrd/select/PUT/exec',
+        r'cat ${SHARED_API_DIR}/analytic/rrd/select/PUT/result | xargs find ${RRD_ROOT} | ${WORK_DIR}/graph_rrd.py ${SHARED_API_DIR}/analytic/rrd/select/plot_view ${RESULT_FILE}',
         r'cat ${RESULT_FILE}.png',
         r'rm -f ${RESULT_FILE}.png'
     )
@@ -96,7 +96,7 @@ def get():
         "rrd_collect": make_script_rrd_collect,
         "rrd_select": make_script_rrd_select,
         "rrd_view": make_script_rrd_view,
-        "rrd_view_graph": make_script_rrd_view_graph
+        "rrd_plot_view": make_script_rrd_view_graph
     }
 
     scripts_help_generator = {
@@ -105,6 +105,6 @@ def get():
         "rrd_collect": generate_script_rrd_collect_help,
         "rrd_select": generate_script_rrd_select_help,
         "rrd_view": generate_script_rrd_view_help,
-        "rrd_view_graph": generate_script_rrd_view_graph_help
+        "rrd_plot_view": generate_script_rrd_view_graph_help
     }
     return scripts_generator, scripts_help_generator
