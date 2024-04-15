4b364bdab06ab0b2e5454a2220531fb2ed5ac7ad
SergeyIvanov87 <sergius140608@rambler.ru>
2024-04-16 00:35:03 +0200

    Finish REST_API tests

--- submodules/rest_api/tests/e2e/init.sh
+++ submodules/rest_api/tests/e2e/init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 export WORK_DIR=${1}
-export INITIAL_PROJECT_LOCATION=${2}
-export SHARED_API_DIR=${3}
+export SHARED_API_DIR=${2}
 
 export UTILS="/main_image_env"
 
+export MAIN_SERVICE_NAME=api.pmccabe_collector.restapi.org
 
+mv ${WORK_DIR}/data/portal.json portal.json
 ${UTILS}/build_api_executors.py ${WORK_DIR}/data ${WORK_DIR} -o ${WORK_DIR}
 ${UTILS}/build_api_services.py ${WORK_DIR}/data ${WORK_DIR} -o ${WORK_DIR}/services
+mkdir -p ${SHARED_API_DIR}/${MAIN_SERVICE_NAME}
 ${UTILS}/make_api_readme.py ${WORK_DIR}/data > ${SHARED_API_DIR}/${MAIN_SERVICE_NAME}/README-API-MOCK.md
 ${UTILS}/build_api_pseudo_fs.py ${WORK_DIR}/data ${SHARED_API_DIR}
 
@@ -20,9 +22,7 @@ for s in ${WORK_DIR}/services/*_server.sh; do
 done
 
 # TODO temporary solution sleep 30:
-sleep 30
-
-export MAIN_SERVICE_NAME=api.pmccabe_collector.restapi.org
+sleep 40
 
 # clear existing generated API schema files to facilitate clear-build environment
 rm -fr ${WORK_DIR}/restored_API
@@ -30,6 +30,7 @@ mkdir -p ${WORK_DIR}/restored_API
 ${UTILS}/restore_api_from_pseudo_fs.py ${SHARED_API_DIR} ${MAIN_SERVICE_NAME} ${WORK_DIR}/restored_API
 
 echo "Run tests:"
+mv portal.json ${WORK_DIR}/restored_API/portal.json
 for s in ${WORK_DIR}/test_*.py; do
     pytest ${s}
 done
