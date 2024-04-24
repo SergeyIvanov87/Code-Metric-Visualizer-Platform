81e05e4de6bad7162664c67a08b8cc8421ce40a2
SergeyIvanov87 <sergius140608@rambler.ru>
2024-04-24 22:45:32 +0200

    Remove non-usable scripts from CC

--- cyclomatic_complexity/build_pmccabe_flamegraph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-XML_NAME=${1}
-FLG_NAME=${2}
-cat ${XML_NAME} | ${WORK_DIR}/pmccabe_visualizer/collapse.py -attr mmcc | ${WORK_DIR}/FlameGraph/flamegraph.pl > ${FLG_NAME}_mmcc.svg
-cat ${XML_NAME} | ${WORK_DIR}/pmccabe_visualizer/collapse.py -attr tmcc | ${WORK_DIR}/FlameGraph/flamegraph.pl > ${FLG_NAME}_tmcc.svg
-cat ${XML_NAME} | ${WORK_DIR}/pmccabe_visualizer/collapse.py -attr sif | ${WORK_DIR}/FlameGraph/flamegraph.pl > ${FLG_NAME}_sif.svg
-cat ${XML_NAME} | ${WORK_DIR}/pmccabe_visualizer/collapse.py -attr lif | ${WORK_DIR}/FlameGraph/flamegraph.pl > ${FLG_NAME}_lif.svg
-cat ${XML_NAME} | ${WORK_DIR}/pmccabe_visualizer/collapse.py -attr mmcc,tmcc,sif,lif | ${WORK_DIR}/FlameGraph/flamegraph.pl > ${FLG_NAME}_all.svg
--- cyclomatic_complexity/build_pmccabe_xml.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-XML_NAME=${1}
-find ${INITIAL_PROJECT_LOCATION} -regex ".*\.\(hpp\|cpp\|c\|h\)" | grep -v "buil" | grep -v "3pp" | grep -v "thirdpart" | ${WORK_DIR}/pmccabe_visualizer/pmccabe_build.py > ${XML_NAME}
