69559ae83c636fd9dc90240fdb396dcf7271bea2
SergeyIvanov87 <sergius140608@rambler.ru>
2024-06-21 09:17:08 +0200

    Conceive REST_API integrational tests

---
+++ rest_api/tests/integrational/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export WORK_DIR=${1}
+export SHARED_API_DIR=${2}
+
+export MAIN_SERVICE_NAME=api.pmccabe_collector.restapi.org
+export PYTHONPATH="${WORK_DIR}:${WORK_DIR}/utils:${WORK_DIR}/utils/modules"
+UTILS="${WORK_DIR}/utils"
+export MODULES="${WORK_DIR}/utils/modules"
+
+# clear existing generated API schema files to facilitate clear-build environment
+rm -fr ${WORK_DIR}/API_for_testing
+mkdir -p ${WORK_DIR}/API_for_testing
+cp -r ${WORK_DIR}/collected_API/* ${WORK_DIR}/API_for_testing/
+
+echo "Run tests:"
+for s in ${WORK_DIR}/test_*.py; do
+    pytest ${s}
+done
+
+echo "wait for termination"
+sleep infinity &
+wait $!
+exit $RET
