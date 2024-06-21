a2a0e3627cd13e13257ea0d28ec1e0301db48b4d
SergeyIvanov87 <sergius140608@rambler.ru>
2024-06-21 09:17:33 +0200

    Conceive RRD integrational tests

---
+++ rrd/tests/integrational/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export WORK_DIR=${1}
+export INITIAL_PROJECT_LOCATION=${2}
+export SHARED_API_DIR=${3}
+
+export PYTHONPATH="${WORK_DIR}:${WORK_DIR}/utils:${WORK_DIR}/utils/modules"
+UTILS="${WORK_DIR}/utils"
+export MODULES="${WORK_DIR}/utils/modules"
+
+export MAIN_SERVICE_NAME=api.pmccabe_collector.restapi.org
+
+echo "Run tests:"
+RET=0
+for s in ${WORK_DIR}/test_*.py; do
+    pytest -s ${s}
+    VAL=$?
+    if [ $VAL != 0 ]
+    then
+        RET=$VAL
+    fi
+done
+
+echo "wait for termination"
+sleep infinity &
+wait $!
+exit $RET
