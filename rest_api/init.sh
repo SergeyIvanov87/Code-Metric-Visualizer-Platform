60e85dc2890a7acd9bd7d490d7f9b2495c72c659
SergeyIvanov87 <sergius140608@rambler.ru>
2024-06-27 11:31:51 +0200

    Improve robustness of REST_API service, stop container due to inoperable status

--- rest_api/init.sh
+++ rest_api/init.sh
@@ -21,24 +21,88 @@ echo "DEBUG CMD:"
 echo "cp ../rest_api_server/rest_api_server/cgi_template.py ../rest_api_server/rest_api_server/cgi.py && ../build_api_cgi.py ../restored_API /api api.pmccabe_collector.restapi.org >> ../rest_api_server/rest_api_server/cgi.py"
 echo "flask --app rest_api_server run --host 0.0.0.0"
 
-/package/watchdog_server.sh &
+# Launch REST_API server in an infinitive loop:
+# It must be started before any inotify event has been listened,
+# so when earlier event was arrived, we would process that
+export REST_API_INSTANCE_PIDFILE=${MY_FLASK_INSTANCE_PIDFILE}
+/package/watchdog_server.sh ${REST_API_INSTANCE_PIDFILE} ${FLASK_RUN_HOST} ${FLASK_RUN_PORT} &
 WATCHDOG_PID=$!
 
 shopt -s extglob
-while :
-do
-    inotifywait -mr ${SHARED_API_DIR} -e modify,create,delete --include '.md$' |
+RETURN_STATUS=0
+# Loop until any unrecoverable error would occur
+while [ $RETURN_STATUS -eq 0 ]; do
+        # Algorithm modifies RETURN_STATUS variable inside this inner loop, which would have been a different subshell if we have launched it in
+        # a accustomed way like:
+        #
+        #      inotifywait -mr ${SHARED_API_DIR} -e modify,create,delete --include '.md$' | while read dir action file; do
+        #
+        # This pipelining "|" creates a new subshell actually, hence given RETURN_STATUS variable would not be changed in the main script rather than its duplicate in a subshell.
+        # To overcome this limitation It had better use `here string` https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Here-Strings,
+        # which allow us to execute this inner loop in the main shell context so that modification of RETURN_STATUS will be visible here
+        # and allow this watching algorithm to be stopped by emergency
         while read dir action file; do
-            echo "event has happened: ${file}, action; ${action}, dir: ${dir}"
+            echo "Event has been detected: ${file}, action; ${action}, dir: ${dir}. Reconfigure REST_API Service...."
             case "$action" in
                 CREATE|DELETE|MODIFY )
-                    date=`date +%Y-%m-%dT%H:%M:%S`
-                    echo "file changed ${file}"
-                    killall python
+                    # In general, it must restart a running server instance by sending SIGTERM to its PID.
+                    # PID is expected to be stored in REST_API_INSTANCE_PIDFILE.
+                    # This file cannot appear instantly, as well as a REST_API server instance recreated, thus we wait for this file appearance.
+                    # The situation is more accute when events occured rapidly...
+                    # Therefore to be consistent, It had better wait until the service starts.
+
+                    # By the way, through an unpredictable error in launching the new server instance may occureded
+                    # to avoid waiting forever, it is OK untill it checks whether WATCHDOG is alive.
+                    # If WATCHDOG is alive then it can handle the situation or gives another try,
+                    # we is about to continue waiting for REST_API service uprising, unless
+                    # WATCHDOG is dead itself. Once the service is dead, this container became inoperable
+                    while [ ! -f ${REST_API_INSTANCE_PIDFILE} ]; do
+                        kill -s 0 ${WATCHDOG_PID} > /dev/null 2>&1
+                        WATCHDOG_TEST_RESULT=$?
+                        if [ $WATCHDOG_TEST_RESULT != 0 ]; then
+                            echo "Unrecoverable error has occured. Container is inoperable. Abort."
+                            RETURN_STATUS=255
+                            break
+                        fi
+                        echo "Waiting for a server to start..."
+                        sleep 1;
+                    done
+
+                    # kill an old instance
+                    REST_API_INSTANCE_PIDFILE_PID=`cat ${REST_API_INSTANCE_PIDFILE}`
+                    kill -s SIGTERM ${REST_API_INSTANCE_PIDFILE_PID}
+                    while [ -f ${REST_API_INSTANCE_PIDFILE} ]; do
+                        sleep 0.1
+                        echo "Waiting for a server to stop, pid ${REST_API_INSTANCE_PIDFILE_PID}..."
+                    done
+                    # wait for a new instance to start and also make sure than WATCHDOG is alive so that
+                    # no any unrecoverable erro has occured.
+                    # If we omit thsi check than next WATCHDOG & service cheking will happen up on another
+                    # inotify event has come, which may be unreachable.
+                    # The solution might be introducing inotify timeout instead, but using that
+                    # we might miss events occured during that timeout
+                    while [ ! -f ${REST_API_INSTANCE_PIDFILE} ]; do
+                        kill -s 0 ${WATCHDOG_PID} > /dev/null 2>&1
+                        WATCHDOG_TEST_RESULT=$?
+                        if [ $WATCHDOG_TEST_RESULT != 0 ]; then
+                            echo "Unrecoverable error has occured. Container is inoperable. Abort."
+                            WATCHDOG_PID=0
+                            RETURN_STATUS=255
+                            break
+                        fi
+                        echo "Waiting for a new server instance to start..."
+                        sleep 1;
+                    done
                     ;;
                 *)
                     ;;
             esac
-        done
+        done <<< "$(inotifywait -r ${SHARED_API_DIR} -e modify,create,delete --include '.md$' )"
 done
-wait ${WATCHDOG_PID}
+
+if [ ! -z ${WATCHDOG_PID} ] && [ ${WATCHDOG_PID} != 0 ]; then
+    echo "Wait for watchdog finished"
+    wait ${WATCHDOG_PID}
+fi
+echo "Exit with code: ${RETURN_STATUS}"
+exit ${RETURN_STATUS}
--- rest_api/watchdog_server.sh
+++ rest_api/watchdog_server.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
 
+LOG_PREFIX="[WATCHDOG]: "
+# file for storing a PID of a server instance
+MY_REST_API_INSTANCE_PIDFILE=${1}
+if [ -z ${MY_REST_API_INSTANCE_PIDFILE} ]; then
+    MY_REST_API_INSTANCE_PIDFILE=/package/rest_api_server_pid
+fi
+
+MY_FLASK_RUN_HOST=${2}
+if [ -z ${MY_FLASK_RUN_HOST} ]; then
+    if [ -z ${FLASK_RUN_HOST} ]; then
+        echo "${LOG_PREFIX}Neither MY_FLASK_RUN_HOST nor FLASK_RUN_HOST are set, it is impossible to launch watchdog without this env-variabled defined. Abort"
+        exit 255
+    fi
+    MY_FLASK_RUN_HOST=${FLASK_RUN_HOST}
+fi
+
+MY_FLASK_RUN_PORT=${3}
+if [ -z ${MY_FLASK_RUN_PORT} ]; then
+    if [ -z ${FLASK_RUN_PORT} ]; then
+        echo "${LOG_PREFIX}Neither MY_FLASK_RUN_PORT nor FLASK_RUN_PORT are set, it is impossible to launch watchdog without this env-variabled defined. Abort"
+        exit 255
+    fi
+    MY_FLASK_RUN_PORT=${FLASK_RUN_PORT}
+fi
+
 let wait_for_file_API_counter=0
 let wait_for_file_API_limit=4
 let wait_for_api_exporting_directory_sec=15
+
+let wait_for_file_API_initializing_by_its_owners_counter=0
+let wait_for_file_API_initializing_by_its_owners_limit=5
+let wait_for_api_initialization_by_its_owners_sec=1
+
+let wait_for_server_starting_counter=0
+let wait_for_server_starting_limit=5
+let wait_for_server_starting_sec=1
+
 while :
 do
+    rm -f ${MY_REST_API_INSTANCE_PIDFILE}
     # wait for API directory creation
     if [ ! -d "${SHARED_API_DIR}/${MAIN_SERVICE_NAME}" ]; then
         if [ ${wait_for_file_API_counter} == ${wait_for_file_API_limit} ]; then
-            echo "API exporting directory has not been served as expected. Abort"
+            echo "${LOG_PREFIX}API exporting directory has not been served as expected. Abort"
+            rm -f ${MY_REST_API_INSTANCE_PIDFILE}
             exit 255
         fi
         let wait_for_file_API_counter=$wait_for_file_API_counter+1
-        echo "API exporting directory: ${SHARED_API_DIR}/${MAIN_SERVICE_NAME} - doesn't exist. Another attempt: ${wait_for_file_API_counter}/${wait_for_file_API_limit} - will be made in ${wait_for_api_exporting_directory_sec} seconds."
+        echo "${LOG_PREFIX}API exporting directory: ${SHARED_API_DIR}/${MAIN_SERVICE_NAME} - doesn't exist. Another attempt: ${wait_for_file_API_counter}/${wait_for_file_API_limit} - will be made in ${wait_for_api_exporting_directory_sec} seconds."
         sleep ${wait_for_api_exporting_directory_sec} &
         wait $!
         continue
@@ -25,33 +61,73 @@ do
     # wait for API files
     if [ `ls ${WORK_DIR}/restored_API/*.json | wc -l` == 0 ]; then
         if [ ${wait_for_file_API_counter} == ${wait_for_file_API_limit} ]; then
-            echo "API exporting directory contains no JSON data. Abort"
+            echo "${LOG_PREFIX}API exporting directory contains no JSON data. Abort"
+            rm -f ${MY_REST_API_INSTANCE_PIDFILE}
             exit 255
         fi
         let wait_for_file_API_counter=$wait_for_file_API_counter+1
-        echo "Nothing was gathered from the API directory: ${SHARED_API_DIR}/${MAIN_SERVICE_NAME}. Please check your exporting API. Another attempt: ${wait_for_file_API_counter}/${wait_for_file_API_limit} - will be made in ${wait_for_api_exporting_directory_sec} seconds."
+        echo "${LOG_PREFIX}Nothing was gathered from the API directory: ${SHARED_API_DIR}/${MAIN_SERVICE_NAME}. Please check your exporting API. Another attempt: ${wait_for_file_API_counter}/${wait_for_file_API_limit} - will be made in ${wait_for_api_exporting_directory_sec} seconds."
         sleep ${wait_for_api_exporting_directory_sec} &
         wait $!
         continue
     fi
 
-    echo "Generate REST_API CGIs from imported API entry point"
+    # wait for Markdown files
+    if [ ! -f ${WORK_DIR}/restored_API/index.md ]; then
+        if [ ${wait_for_file_API_initializing_by_its_owners_counter} == ${wait_for_file_API_initializing_by_its_owners_limit} ]; then
+            echo "${LOG_PREFIX}Can't find API README from'${WORK_DIR}/restored_API/index.md' - probably filesystem API is inconsistent. REST_API server won't serve any request and will be stopped"
+            rm -f ${MY_REST_API_INSTANCE_PIDFILE}
+            exit 255
+        fi
+        let wait_for_file_API_initializing_by_its_owners_counter=$wait_for_file_API_initializing_by_its_owners_counter+1
+        echo "${LOG_PREFIX}File ${WORK_DIR}/restored_API/index.md hasn't been recreated yet. Probably APIs were not initialized entirely. Another attempt: ${wait_for_file_API_initializing_by_its_owners_counter}/${wait_for_file_API_initializing_by_its_owners_limit} - will be made in ${wait_for_api_initialization_by_its_owners_sec} seconds."
+        sleep ${wait_for_api_initialization_by_its_owners_sec} &
+        wait $!
+        continue
+    fi
+
+    echo "${LOG_PREFIX}Generate REST_API CGIs from imported API entry point"
     rm -f ${WORK_DIR}/rest_api_server/rest_api_server/cgi.py
     cp ${WORK_DIR}/rest_api_server/rest_api_server/cgi_template.py ${WORK_DIR}/rest_api_server/rest_api_server/cgi.py
-
-    rm -f ${WORK_DIR}/restored_API/index.md
     ${WORK_DIR}/build_api_cgi.py ${WORK_DIR}/restored_API ${SHARED_API_DIR} ${MAIN_SERVICE_NAME} >> ${WORK_DIR}/rest_api_server/rest_api_server/cgi.py
-
-    if [ ! -f ${WORK_DIR}/restored_API/index.md ]; then
-        echo "Can't find API README from'${WORK_DIR}/restored_API/index.md' - probably filesystem API is inconsistent. REST_API server won't serve any request and will be stopped"
-        exit 255
-    fi
     cp ${WORK_DIR}/restored_API/index.md  ${WORK_DIR}/rest_api_server/rest_api_server/templates/
     cd ${WORK_DIR}/rest_api_server/
 
-    echo "Start REST_API server"
+    echo "${LOG_PREFIX}Start REST_API server"
     pipx install -e .
-
     python -m flask run &
-    wait $!
+    SERVER_INSTANCE_PID=$!
+    SERVER_INSTANCE_RET=$?
+    if [ ${SERVER_INSTANCE_RET} != 0 ]; then
+        echo "${LOG_PREFIX}Cannot start the server instance, error: ${SERVER_INSTANCE_RET}"
+        exit 255
+    fi
+
+    let wait_for_file_API_counter=0
+    let wait_for_file_API_initializing_by_its_owners_counter=0
+    wait_for_server_starting_counter=0
+    SERVICE=http://${MY_FLASK_RUN_HOST}:${MY_FLASK_RUN_PORT}
+    echo "${LOG_PREFIX}Waiting for a new server instance being initialed on: ${SERVICE}..."
+    while :
+    do
+        curl -I -s -o /dev/null ${SERVICE}
+        if [ $? -eq 0 ];
+        then
+            break
+        fi
+
+        if [ ${wait_for_server_starting_counter} == ${wait_for_server_starting_limit} ]; then
+            echo "${LOG_PREFIX}Service ${SERVICE} has not been initialized. Abort"
+            exit 255
+        fi
+        let wait_for_server_starting_counter=$wait_for_server_starting_counter+1
+        echo "${LOG_PREFIX}Service ${SERVICE} is offline. Another attempt: ${wait_for_server_starting_counter}/${wait_for_server_starting_limit} - will be made in ${wait_for_server_starting_sec} seconds."
+        sleep ${wait_for_server_starting_sec} &
+        wait $!
+        continue
+    done
+
+    echo ${SERVER_INSTANCE_PID} > ${MY_REST_API_INSTANCE_PIDFILE}
+    echo "${LOG_PREFIX}The new server instance has been started, PID: ${SERVER_INSTANCE_PID}"
+    wait ${SERVER_INSTANCE_PID}
 done
