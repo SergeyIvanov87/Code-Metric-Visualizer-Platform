fe11471a1e148035c36143774833773ade8951de
SergeyIvanov87 <sergius140608@rambler.ru>
2024-07-03 00:05:34 +0200

    Add api_fs_query

--- common/images/unit_test/test_2_multi_sessions.py
+++ common/images/unit_test/test_2_multi_sessions.py
@@ -12,6 +12,7 @@ from settings import Settings
 from utils import get_api_queries
 from utils import compose_api_queries_pipe_names
 from async_executor import AsyncExecutor
+from api_fs_query import APIQuery
 
 global_settings = Settings()
 testdata = list(get_api_queries("/API", global_settings.domain_name_api_entry).items())
@@ -33,9 +34,10 @@ class APIExecutor(AsyncExecutor):
             exec_params = "SESSION_ID=" + session_id_value
 
             # ask for a new pipe pairs every time once session id changed
-            communication_pipes = compose_api_queries_pipe_names(
+            query = APIQuery(compose_api_queries_pipe_names(
                 obj.api_mount_directory, obj.query, session_id_value
-            )
+            ))
+            assert query.wait_until_valid(0.1, 30, True), f"Pipes for test {name} must have been created"
 
             # check API transaction
             print(
@@ -43,35 +45,20 @@ class APIExecutor(AsyncExecutor):
                 file=sys.stdout,
                 flush=True,
             )
-            with open(communication_pipes[0], "w") as pin:
-                pin.write(exec_params + "\n")
 
+            query.execute(exec_params)
             # wait for output pipe creation
-            # wating timeout  might be increasedhere, because multiple threads
-            # occupies more CPU time and a shceduler might supercede pipe creation server process
-            api_result_pipe_timeout_cycles = 0
-            api_result_pipe_timeout_cycles_limit = 100
-            while not (
-                os.path.exists(communication_pipes[1])
-                and stat.S_ISFIFO(os.stat(communication_pipes[1]).st_mode)
-            ):
-                time.sleep(0.1)
-                api_result_pipe_timeout_cycles += 1
-                if (
-                    api_result_pipe_timeout_cycles
-                    >= api_result_pipe_timeout_cycles_limit
-                ):
-                    obj.error_message += f'ERROR: Output pipe "{communication_pipes[1]}" hasn\'t been created in designated interval {0.1 * float(api_result_pipe_timeout_cycles_limit)}sec'
-                    break
+            # wating timeout  might be increased here, because multiple threads
+            # occupies more CPU time and a scheduler might supercede a pipe creation server process errand
+            # by other activity
+            result = ""
+            try:
+                result = query.wait_result(session_id_value, 0.1, 100, True)
+            except RuntimeError as timeout:
+                obj.error_message += f'ERROR: Response pipe for session "{session_id_value_base}" hasn\'t been created in designated interval {0.1 * float(100)}sec'
 
-            if len(obj.error_message):
-                break
-
-            with open(communication_pipes[1], "r") as pout:
-                if not len(pout.read()):
-                    obj.error_message += f'ERROR: Got empty result from the output pipe "{communication_pipes[1]}"'
-                    break
-                continue
+            if result == "":
+                obj.error_message += f'ERROR: Got empty result from the session "{session_id_value_base}"'
 
             if len(obj.error_message):
                 break
