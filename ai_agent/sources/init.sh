c71e3aa80814b2e0a3208e9cbc4f65c645e77908
SergeyIvanov87 <sergius140608@rambler.ru>
2026-04-22 09:21:15 +0200

    Generate container artefacts in a single folder, Add .gitignore file

--- ai_agent/sources/init.sh
+++ ai_agent/sources/init.sh
@@ -8,14 +8,18 @@ export PYTHONPATH="${3}:${3}/modules"
 
 export MAIN_SERVICE_NAME=api.pmccabe_collector.restapi.org
 
-export INNER_API_SCHEMA_DIR=${WORK_DIR}/API
+# generate assets in a tmp directory
+export WORK_DIR_TMP=${WORK_DIR}/generated
+mkdir -p ${WORK_DIR_TMP}
+
+export INNER_API_SCHEMA_DIR=${WORK_DIR_TMP}/API
 
 README_FILE_PATH=${SHARED_API_DIR}/${MAIN_SERVICE_NAME}/ai_agent/README-API-STATISTIC.md
 
 # use source this script as fast way to setup environment for debugging
-echo -e "export WORK_DIR=${WORK_DIR}\nexport INITIAL_PROJECT_LOCATION=${INITIAL_PROJECT_LOCATION}\nexport OPT_DIR=${OPT_DIR}\nexport SHARED_API_DIR=${SHARED_API_DIR}\nexport MAIN_SERVICE_NAME=${MAIN_SERVICE_NAME}\nexport INNER_API_SCHEMA_DIR=${INNER_API_SCHEMA_DIR}\nexport PYTHONPATH=${PYTHONPATH}" > ${WORK_DIR}/env.sh
+echo -e "export WORK_DIR=${WORK_DIR}\nexport WORK_DIR_TMP=${WORK_DIR_TMP}\nexport INITIAL_PROJECT_LOCATION=${INITIAL_PROJECT_LOCATION}\nexport OPT_DIR=${OPT_DIR}\nexport SHARED_API_DIR=${SHARED_API_DIR}\nexport MAIN_SERVICE_NAME=${MAIN_SERVICE_NAME}\nexport INNER_API_SCHEMA_DIR=${INNER_API_SCHEMA_DIR}\nexport PYTHONPATH=${PYTHONPATH}" > ${WORK_DIR_TMP}/env.sh
 
-cp -r /API ${WORK_DIR}
+cp -r /API ${WORK_DIR_TMP}
 source ${OPT_DIR}/shell_utils/init_utils.sh
 
 echo "Premature cleanup..."
@@ -45,12 +49,12 @@ termination_handler(){
 trap "termination_handler" SIGHUP SIGQUIT SIGABRT SIGKILL SIGALRM SIGTERM EXIT
 
 # TODO consider use launch_inner_api_services!
-${OPT_DIR}/build_api_executors.py ${INNER_API_SCHEMA_DIR} ${WORK_DIR} -o ${WORK_DIR}
-${OPT_DIR}/build_api_services.py ${INNER_API_SCHEMA_DIR} ${WORK_DIR} -o ${WORK_DIR}/services
+${OPT_DIR}/build_api_executors.py ${INNER_API_SCHEMA_DIR} ${WORK_DIR} -o ${WORK_DIR_TMP}
+${OPT_DIR}/build_api_services.py ${INNER_API_SCHEMA_DIR} ${WORK_DIR_TMP} -o ${WORK_DIR_TMP}/services
 doas -u root env PYTHONPATH=${PYTHONPATH} SHARED_API_DIR=${SHARED_API_DIR} MAIN_SERVICE_NAME=${MAIN_SERVICE_NAME} ${OPT_DIR}/build_api_pseudo_fs.py ${INNER_API_SCHEMA_DIR} ${SHARED_API_DIR}
 doas -u root chown -R $USER:$GROUPNAME ${SHARED_API_DIR}/${MAIN_SERVICE_NAME}/ai_agent
 
-launch_fs_api_services SERVICE_WATCH_PIDS "${WORK_DIR}/services/"
+launch_fs_api_services SERVICE_WATCH_PIDS "${WORK_DIR_TMP}/services/"
 
 echo -e "${BBlue}Populating README file...${Color_Off}"
 ${OPT_DIR}/make_api_readme.py ${INNER_API_SCHEMA_DIR}  | ( umask 0033; cat >> ${README_FILE_PATH} )
