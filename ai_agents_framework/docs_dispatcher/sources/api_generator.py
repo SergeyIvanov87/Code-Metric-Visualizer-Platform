2ba4755f15202432eb14f4935878a1129276fd79
SergeyIvanov87 <sergius140608@rambler.ru>
2026-05-26 11:51:28 +0200

    Separate Dev & Prod container setups, Add functional tests for docs_dispatcher

--- ai_agents_framework/docs_dispatcher/sources/api_generator.py
+++ ai_agents_framework/docs_dispatcher/sources/api_generator.py
@@ -24,7 +24,7 @@ def make_attach_doc_chunk(script, desired_file_ext=""):
         *api_fs_exec_utils.generate_read_api_fs_args(), r"",
         r'documen_data="${OVERRIDEN_CMD_ARGS[@]: -1}"',
         r'all_others="${OVERRIDEN_CMD_ARGS[@]::${#OVERRIDEN_CMD_ARGS[@]}-2}"',
-        "echo \"${all_others[@]}\" | xargs -I% -- sh -c \"echo '${documen_data}' | ${WORK_DIR}/put_doc.py %\""
+        "echo \"${all_others[@]}\" | xargs -I% -- sh -c \"echo '${documen_data}' | ${WORK_DIR}/attach_doc_chunk.py %\""
     )
     script.writelines(line + "\n" for line in body)
 
