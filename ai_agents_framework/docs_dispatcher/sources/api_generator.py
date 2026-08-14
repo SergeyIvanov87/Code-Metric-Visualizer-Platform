74f28e917640c86c222880f0948d6e4909278548
SergeyIvanov87 <sergius140608@rambler.ru>
2026-08-14 11:19:17 +0200

    Add a new API query get_doc_info

--- ai_agents_framework/docs_dispatcher/sources/api_generator.py
+++ ai_agents_framework/docs_dispatcher/sources/api_generator.py
@@ -140,10 +140,35 @@ def make_get_docs_help():
     return "make_get_docs_help"
 
 
+def make_get_doc_info(script, desired_file_ext=""):
+    if len(desired_file_ext) == 0:
+        file_extension = ".json"
+    else:
+        file_extension = "." + desired_file_ext
+
+    body = (
+        *api_fs_exec_utils.generate_exec_header(), r"",
+        *api_fs_bash_utils.generate_extract_attr_value_from_string(), r"",
+        *api_fs_bash_utils.generate_add_suffix_if_exist(), r"",
+        *api_fs_bash_utils.generate_wait_until_pipe_exist(), r"",
+        *api_fs_exec_utils.generate_get_result_type(file_extension), r"",
+        *api_fs_exec_utils.generate_api_node_env_init(), r"",
+        api_fs_bash_utils.extract_attr_value_from_string() + " \"SESSION_ID\" \"${2}\" \"\" '=' SESSION_ID_VALUE", r"",
+        *api_fs_exec_utils.generate_read_api_fs_args(), r"",
+        r'echo "${OVERRIDEN_CMD_ARGS[@]}" | xargs ${WORK_DIR}/get_doc_info.py'
+    )
+    script.writelines(line + "\n" for line in body)
+
+
+def make_get_doc_info_help():
+    return "make_get_doc_info_help"
+
+
 def get():
     scripts_generator = {
         "attach_doc_chunk": make_attach_doc_chunk,
         "delete_doc": make_delete_doc,
+        "get_doc_info": make_get_doc_info,
         "get_docs": make_get_docs,
         "put_doc": make_put_doc,
         "sync": make_sync
@@ -152,6 +177,7 @@ def get():
     scripts_help_generator = {
         "attach_doc_chunk": make_attach_doc_chunk_help,
         "delete_doc": make_delete_doc_help,
+        "get_doc_info": make_get_doc_info_help,
         "get_docs": make_get_docs_help,
         "put_doc": make_put_doc_help,
         "sync": make_sync_help
