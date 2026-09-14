20f756f8fd2c8f96f22c6cff1cd610b4716621a2
Sergey <35331225+SergeyIvanov87@users.noreply.github.com>
2026-09-14 14:00:24 +0300

    Unmet deps fix (#100)
    * Remove a tmp directory in unmet-deps tests which is presumably exists and is the reasom why functional tests fail launched together
    
    * Use hostname as a unique identifier for creating a tmp directory for inner api tests

--- common/tests/test_1_inner_api_functionality.py
+++ common/tests/test_1_inner_api_functionality.py
@@ -4,6 +4,7 @@ import json
 import os
 import pathlib
 import pytest
+import socket
 import stat
 import time
 import glob, shutil
@@ -47,7 +48,7 @@ def check_all_dependencies_api(query, pipes):
     except Exception as e:
         assert 0
 
-def check_unmet_dependencies_api(query, pipes):
+def check_unmet_dependencies_api(query, pipes, hostname):
     print(f"{get_timestamp()}\tinitiate test query: {query["Query"]}")
     api_query = APIQuery(pipes)
     api_query.execute()
@@ -74,10 +75,15 @@ def check_unmet_dependencies_api(query, pipes):
                 "/api", req_api, req_type)
 
             time_millisecond = round(time.time() * 1000)
-            temporary_replaced_result_pipe_file_path = os.path.join(api_exec_node_directory, "../", str(time_millisecond))
+            temporary_replaced_result_pipe_file_path = os.path.join(api_exec_node_directory, "../", hostname, str(time_millisecond))
+
+            # Ensure the temporary directory doesn't exist before creating it
+            if os.path.exists(temporary_replaced_result_pipe_file_path):
+                shutil.rmtree(temporary_replaced_result_pipe_file_path, ignore_errors=True)
+
             os.makedirs(temporary_replaced_result_pipe_file_path, exist_ok=True)
             pipes_to_move_search_glob = ("result." + result_pipe_ext + "*") if result_pipe_ext != "" else "result*"
-            for file in glob.glob(os.path.join(api_exec_node_directory,pipes_to_move_search_glob)):
+            for file in glob.glob(os.path.join(api_exec_node_directory, pipes_to_move_search_glob)):
                 print(f"{get_timestamp()}\tmove pipe {file} temporary to a new place {temporary_replaced_result_pipe_file_path}")
                 try:
                     shutil.move(file, temporary_replaced_result_pipe_file_path)
@@ -125,9 +131,11 @@ def test_inner_api(name, query):
     # compose expected pipe names, based on query data
     pipes = compose_api_queries_pipe_names(global_settings.api_dir, query)
 
+    hostname = socket.gethostname()
+
     if name == "all_dependencies":
         check_all_dependencies_api(query, pipes)
     elif name == "unmet_dependencies":
-        check_unmet_dependencies_api(query, pipes)
+        check_unmet_dependencies_api(query, pipes, hostname)
     else:
         assert 0
