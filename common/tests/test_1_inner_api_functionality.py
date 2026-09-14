597ede3ad878136d850792301b1e0ea94f827a41
SergeyIvanov87 <sergius140608@rambler.ru>
2026-09-14 12:34:45 +0200

    Remove a tmp directory in unmet-deps tests which is presumably exists and is the reasom why functional tests fail launched together

--- common/tests/test_1_inner_api_functionality.py
+++ common/tests/test_1_inner_api_functionality.py
@@ -75,9 +75,14 @@ def check_unmet_dependencies_api(query, pipes):
 
             time_millisecond = round(time.time() * 1000)
             temporary_replaced_result_pipe_file_path = os.path.join(api_exec_node_directory, "../", str(time_millisecond))
+
+            # Ensure the temporary directory doesn't exist before creating it
+            if os.path.exists(temporary_replaced_result_pipe_file_path):
+                shutil.rmtree(temporary_replaced_result_pipe_file_path, ignore_errors=True)
+
             os.makedirs(temporary_replaced_result_pipe_file_path, exist_ok=True)
             pipes_to_move_search_glob = ("result." + result_pipe_ext + "*") if result_pipe_ext != "" else "result*"
-            for file in glob.glob(os.path.join(api_exec_node_directory,pipes_to_move_search_glob)):
+            for file in glob.glob(os.path.join(api_exec_node_directory, pipes_to_move_search_glob)):
                 print(f"{get_timestamp()}\tmove pipe {file} temporary to a new place {temporary_replaced_result_pipe_file_path}")
                 try:
                     shutil.move(file, temporary_replaced_result_pipe_file_path)
